756b799756880c89308858174a91276d21b846cd
pangdudu1 <1766499298@qq.com>
2020-04-27 18:01:03 +0800

    123

---
+++ demo.js
@@ -0,0 +1,37 @@
+{
+    "Success": true, 
+    "Error": "", 
+    "Data": {
+        "list": [
+            {
+                "amount": "88,888", 
+                "user_grade": "1", 
+                "id": "234", 
+                "name": "3-3æäº§ååæå¡è´¹", 
+                "time_unit": "1"
+            }, 
+            {
+                "amount": "90,000", 
+                "user_grade": "1", 
+                "id": "240", 
+                "name": "3-4æåæå¡è´¹", 
+                "time_unit": "1"
+            }, 
+            {
+                "amount": "20,000,000", 
+                "user_grade": "1", 
+                "id": "209", 
+                "name": "æµè¯4", 
+                "time_unit": "1"
+            }, 
+            {
+                "amount": "32,323,232", 
+                "user_grade": "1", 
+                "id": "177", 
+                "name": "ååå", 
+                "time_unit": "1"
+            }
+        ], 
+        "GradeId": "1"
+    }
+}
